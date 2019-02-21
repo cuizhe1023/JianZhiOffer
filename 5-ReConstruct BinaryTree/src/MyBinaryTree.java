0f77e6879fefcaa4522908429b60d771c23a9698
cuizhe1023 <fight.1214@qq.com>
2019-02-21 12:32:00 +0800

    剑指offer第四题：重建二叉树

---
+++ 5-ReConstruct BinaryTree/src/MyBinaryTree.java
@@ -0,0 +1,17 @@
+/**
+ * @Author: cuizhe
+ * @Date: 2019/2/20 22:17
+ */
+public class MyBinaryTree {
+    public TreeNode root;
+
+    public void insert(TreeNode left,int data,TreeNode right){
+        TreeNode newNode = new TreeNode(data);
+        if (root==null){
+            root=newNode;
+        }else {
+
+        }
+    }
+
+}
---
+++ 5-ReConstruct BinaryTree/src/TreeNode.java
@@ -0,0 +1,14 @@
+/**
+ * @Author: cuizhe
+ * @Date: 2019/2/20 22:16
+ */
+public class TreeNode {
+
+    Integer val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(Integer val) {
+        this.val = val;
+    }
+}
